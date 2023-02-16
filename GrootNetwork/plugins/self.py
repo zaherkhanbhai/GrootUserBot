ee32b2a62ad77a6a57e8f2d713386e45469fd297
zaherkhanbhai <108471217+zaherkhanbhai@users.noreply.github.com>
2023-02-16 21:30:47 +0530

    Create self.py

---
+++ GrootNetwork/plugins/self.py
@@ -0,0 +1,24 @@
+import os
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from GrootNetwork.modules.clientbot.clientbot import client
+from GrootNetwork.modules.helpers.command import commandpro
+from GrootNetwork.modules.helpers.decorators import sudo_users_only, errors
+from GrootNetwork.utilities.misc import SUDOERS
+
+@Client.on_message(commandpro(["op", "x", ".op", "wow", "nice", "beautiful"]) & filters.me)
+async def downloader(_, message: Message):
+    targetcontent = message.reply_to_message
+    downloadtargetcontent = await client.download_media(targetcontent)
+    send = await client.send_document("me", downloadtargetcontent)
+    os.remove(downloadtargetcontent)
+
+
+__MODULE__ = " ☆ Sᴇʟғ ☆"
+__HELP__ = f"""
+**🌱 Dᴏᴡɴʟʟᴏᴀᴅ Aɴʏ Sᴇʟғ-Dᴇsᴛʀᴜᴄᴛ Mᴇᴅɪᴀ Aɴᴅ Sᴀᴠᴇ Iᴛ Tᴏ Yᴏᴜʀ Sᴀᴠᴇ Mᴇssᴀɢᴇ **
+
+**ᴜsᴀɢᴇ:**
+`op|.op` - **Rᴇᴘʟʏ Tᴏ Sᴇʟғ-Dᴇsᴛʀᴜᴄᴛ Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Tᴏ Dᴏᴡɴʟᴏᴀᴅ.**
+"""
